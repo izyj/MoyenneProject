910535b78114e629893c86ceac651593f2ad10ff
Ivan Klarman <klarman.ivan@gmail.com>
2015-06-25 22:23:42 +0200

    icon construct

--- app/src/main/java/esgi/ikji/mamoyenne/MainActivity.java
+++ app/src/main/java/esgi/ikji/mamoyenne/MainActivity.java
@@ -69,6 +69,9 @@ public class MainActivity extends ActionBarActivity {
 		bt_del.setOnClickListener(new View.OnClickListener() {
 
 			public void onClick(View v) {
+
+				
+
 				/**
 				 * Suppression en Base de donnees
 				 */
