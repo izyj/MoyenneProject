e98adac1b19b4f35bbba577f22e64461fb74292c
izyj <iisiramen@gmail.com>
2015-06-28 23:08:01 +0200

    correction

--- app/build/generated/source/r/debug/esgi/ikji/mamoyenne/R.java
+++ app/build/generated/source/r/debug/esgi/ikji/mamoyenne/R.java
@@ -1410,9 +1410,8 @@ containing a value of this type.
         public static final int abc_simple_dropdown_hint=0x7f030016;
         public static final int activity_main=0x7f030017;
         public static final int fragment_first=0x7f030018;
-        public static final int fragment_main=0x7f030019;
-        public static final int fragment_presentation=0x7f03001a;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001b;
+        public static final int fragment_presentation=0x7f030019;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001a;
     }
     public static final class menu {
         public static final int main=0x7f0c0000;
@@ -1437,7 +1436,8 @@ containing a value of this type.
         public static final int abc_toolbar_collapse_description=0x7f0a000f;
         public static final int action_settings=0x7f0a0010;
         public static final int app_name=0x7f0a0011;
-        public static final int hello_world=0x7f0a0012;
+        public static final int black=0x7f0a0012;
+        public static final int title_Application=0x7f0a0013;
     }
     public static final class style {
         /** 
